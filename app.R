03b45cc7a6105ad0cb63366ad05fbd126f952cb0
jhk0530 <hwanistic@gmail.com>
2020-03-12 22:17:35 +0900

    Update app.R
    modified variables

--- app.R
+++ app.R
@@ -35,126 +35,164 @@ myButton <- function(inputId, label, width = NULL, onClick = NULL, ...) {
 
 
 triage <- function(v) {
-  age <- disease <- temperature <- count <- oxygen <- pressure <- breath <- 0
+  
+  PT = PCF = PCO = PM = 0
+  
+  ##### PT
+  
+  T = v$T
+  if(T <= 35) PT = 3 
+  if(T <= 36 || T >39) PT = max(2,PT)
+  if(T >= 38 ) PT = max(1,PT)
 
-  if (v$Age >= 60) age <- 1
-  if (v$Age >= 70) age <- 2
-  if (v$Age >= 80) age <- 3
-
-  if (v$Disease) disease <- 3
-
-  if (v$Temperature <= 36) temperature <- 1
-  if (v$Temperature >= 37.5) temperature <- 1
-  if (v$Temperature > 38) temperature <- 2
-  if (v$Temperature > 39) temperature <- 3
-
-  if (v$BreathCount <= 11) count <- 1
-  if (v$BreathCount <= 8) count <- 3
-
-  if (v$Oxygen <= 95) oxygen <- 1
-  if (v$Oxygen <= 93) oxygen <- 2
-  if (v$Oxygen <= 91) oxygen <- 3
-
-  if (v$BloodPressure <= 110) pressure <- 1
-  if (v$BloodPressure <= 100) pressure <- 2
-  if (v$BloodPressure <= 90) pressure <- 3
-
-  breath <- as.integer((breath %in% 9:11) + 2 * (breath <= 8))
-
-  point <- sum(age, disease, temperature, count, oxygen, pressure, breath)
-
-  return(point)
+  ##### PCF
+  HB = v$HB
+  DHB = v$DHB
+  CP = v$CP
+  F = v$F
+  C = v$C
+  BL = v$BL
+  HHB = v$HHB
+  
+  if(HB){
+    if(sum(DHB, CP, F, C, BL, HHB)) PCF = 3
+    else PCF = 2
+  }
+  
+  O = v$O
+  if(O<=93) PCF = 3
+  if(O<=95) PCF = max(2,PCF)
+  
+  BC = v$BC
+  if(BC >=25 || BC <=8) PCF = 3
+  if(BC >=21 || BC <=11) PCF = max(2,PCF)
+  
+  P = v$P
+  if(P > 110 || P <= 40) PCF = 3
+  if(P >= 100 ) PCF = max(2,PCF)
+  if(P >= 101 || P <= 110) PCF = max(2,PCF)
+  
+  ##### PCO
+  
+  CO = v$CO
+  if(!CO) PCO = 3
+  
+  ##### PM
+  
+  PO=v$PO
+  PE=v$PE	
+  TR=v$TR	
+  CH=v$CH	
+  CC=v$CC	
+  AC=v$AC	
+  W=v$W	
+  SA=v$SA	
+  FE=v$FE
+  
+  if(sum(PO,PE,TR,C,CC,AC,W,SA,FE)>=4) PM = 3
+  else if(sum(PO,PE,TR,C,CC,AC,W,SA,FE)>=1) PM = max(1,PM)
+  
+  return(c(PT, PCF, PCO, PM, max(PT, PCF, PCO, PM)))
 }
 
-ui <- material_page(
-  nav_bar_color = "deep-purple lighten-1",
-  color = "#311b92",
-  title = "corona-triage <a href = 'https://github.com/shinykorea/corona-triage' target='_blank'> <i class='material-icons' style = 'font-size:1.3em;'> info </i> </a>",
-  useShinyjs(),
-  tags$head(tags$style(type = "text/css", "table.dataTable tr.selected td, table.dataTable td.selected {background-color: #d1c4e9 !important;}")),
-  tags$head(tags$style(type = "text/css", "th, td {text-align:center !important;}")),
-  tags$head(tags$style(type = "text/css", "html, body {height:100% !important;}")),
-  tags$head(tags$style(type = "text/css", ".indicator {height:0.3em;}")),
-  tags$head(tags$style(type = "text/css", "table {table-layout:fixed; width:100%;}")),
-  tags$head(tags$style(type = "text/css", ".container-fab {height: 3em; background-color: #7e57c2;}")),
-  tags$head(tags$style(type = "text/css", ".btn.btn-default.action-button.buttons-fab.shiny-bound-input { background-color: #b388ff;}")),
-  tags$head(tags$style(type = "text/css", ".tabs .tab a, .tabs .tab a:hover, .tabs .tab a.active {font-size:1.5em; color : #311b92;}")),
-  material_tabs(
-    tabs = c(
-      "자가" = "home",
-      "생활치료센터" = "facility"
+ui <- function(){
+  material_page(
+    nav_bar_color = "deep-purple lighten-1",
+    color = "#311b92",
+    title = "corona-triage <a href = 'https://github.com/shinykorea/corona-triage' target='_blank'> <i class='material-icons' style = 'font-size:1.3em;'> info </i> </a>",
+    useShinyjs(),
+    tags$head(tags$style(type = "text/css", "table.dataTable tr.selected td, table.dataTable td.selected {background-color: #d1c4e9 !important;}")),
+    tags$head(tags$style(type = "text/css", "th, td {text-align:center !important;}")),
+    tags$head(tags$style(type = "text/css", "html, body {height:100% !important;}")),
+    tags$head(tags$style(type = "text/css", ".indicator {height:0.3em;}")),
+    tags$head(tags$style(type = "text/css", "table {table-layout:fixed; width:100%;}")),
+    tags$head(tags$style(type = "text/css", ".container-fab {height: 3em; background-color: #7e57c2;}")),
+    tags$head(tags$style(type = "text/css", ".btn.btn-default.action-button.buttons-fab.shiny-bound-input { background-color: #b388ff;}")),
+    tags$head(tags$style(type = "text/css", ".tabs .tab a, .tabs .tab a:hover, .tabs .tab a.active {font-size:1.5em; color : #311b92;}")),
+    material_tabs(
+      tabs = c(
+        "자가" = "home",
+        "생활치료센터" = "facility"
+      ),
+      color = "#311b92"
     ),
-    color = "#311b92"
-  ),
-
-  # Define tab content
-  material_tab_content(
-    tab_id = "home",
-    tags$h1("자가"),
-    div( # navigator
-      material_column(
-        myButton(inputId = 'totab1',label = 'tab1',onClick = 'location.href = "#tab1"'),
-        myButton(inputId = 'tomap',label = 'map',onClick = 'location.href = "#map"'),
-        myButton(inputId = 'totab2',label = 'tab2',onClick = 'location.href = "#tab2"'),
-        myButton(inputId = 'toimg',label = 'img',onClick = 'location.href = "#img"'),
+    
+    # Define tab content
+    material_tab_content(
+      tab_id = "home",
+      tags$h1("자가"),
+      div( # navigator
+        material_column(
+          myButton(inputId = 'totab1',label = 'tab1',onClick = 'location.href = "#tab1"'),
+          myButton(inputId = 'tomap',label = 'map',onClick = 'location.href = "#map"'),
+          myButton(inputId = 'totab2',label = 'tab2',onClick = 'location.href = "#tab2"'),
+          myButton(inputId = 'toimg',label = 'img',onClick = 'location.href = "#img"'),
         ),
-      id = 'home_navigator',
-      style = 'position:fixed;bottom:3em;width:100%;z-index:999;'
+        id = 'home_navigator',
+        style = 'position:fixed;bottom:3em;width:100%;z-index:999;'
+        
+      ),
+      material_row(
+        material_column(
+          width = 12,
+          material_card(
+            depth = 3,
+            fileInput(
+              inputId = "file",
+              label = tags$a(href = "https://github.com/shinykorea/corona-triage/blob/master/Example.xlsx?raw=true", tags$div(HTML(paste("엑셀(xlsx)", tags$span(style = "color:black", "업로드"), sep = "")))),
+              accept = ".xlsx",
+              multiple = FALSE
+            ),
+            
+            DT::dataTableOutput("tab1", width = "100%")
+          )
+        ),
+        material_column(
+          width = 12,
+          material_card(
+            tags$h2("시군구 지도 필요", id = 'map'),
+            p("자가: 시군구 보건소에서 매일 데이터 보내주는 시나리오."),
+            p("우리는 데이터를 받아서 당일 업데이트 현황을 지도에 보여줘야 함."),
+            p("예) 당일 업데이트된 사람 60%, 50% 미만이면 빨간색"),
+            depth = 3
+          )
+        )
+      ),
+      
+      material_row(
+        height = "100%",
+        material_column(
+          material_card(
+            title = htmlOutput("pat", style = "text-align:center"),
+            divider = TRUE,
+            DT::dataTableOutput("tab2")
+          ),
+          width = 12
+        ),
+            
+        material_column(
+          material_card(
+            title = HTML(paste0('Visualize among time. ',
+                          '<span style = "background-color:#EA2027">Red</span> : 3, ',
+                         '<span style = "background-color:#F79F1F">Orange</span> : 2, ',
+                         '<span style = "background-color:#FFC312">Yellow</span> : 1, ',
+                         '<span style = "background-color:#A3CB38">Green</span> : 0')),
+            highchartOutput("img", height = "600px"),
+            depth = 3
+          ),
+          width = 12
+        )
+      ),
+      div(style = "height:3em;")
       
     ),
-    material_row(
-      material_column(
-        width = 12,
-        material_card(
-          depth = 3,
-          fileInput(
-            inputId = "file",
-            label = tags$a(href = "https://github.com/shinykorea/corona-triage/blob/master/Example.xlsx?raw=true", tags$div(HTML(paste("엑셀(xlsx)", tags$span(style = "color:black", "업로드"), sep = "")))),
-            accept = ".xlsx",
-            multiple = FALSE
-          ),
-
-          DT::dataTableOutput("tab1", width = "100%")
-        )
-      ),
-      material_column(
-        width = 12,
-        material_card(
-          tags$h2("시군구 지도 필요", id = 'map'),
-          p("자가: 시군구 보건소에서 매일 데이터 보내주는 시나리오."),
-          p("우리는 데이터를 받아서 당일 업데이트 현황을 지도에 보여줘야 함."),
-          p("예) 당일 업데이트된 사람 60%, 50% 미만이면 빨간색"),
-          depth = 3
-        )
-      )
-    ),
-
-    material_row(
-      height = "100%",
-      material_column(
-        material_card(
-          title = htmlOutput("pat", style = "text-align:center"),
-          divider = TRUE,
-          DT::dataTableOutput("tab2")
-        ),
-        width = 12
-      ),
-      material_column(
-        material_card(
-          highchartOutput("img", height = "600px"),
-          depth = 3
-        ),
-        width = 12
-      )
-    ),
-    div(style = "height:3em;")
-  
-  ),
-  material_tab_content(
-    tab_id = "facility",
-    tags$h2("생활치료센터")
+    material_tab_content(
+      tab_id = "facility",
+      tags$h2("생활치료센터")
+    )
   )
-)
+}
+  
 
 # Wrap your UI with secure_app
 # ui <- secure_app(ui, enable_admin = T)  개발땐 생략, 배포시 적용
@@ -168,58 +206,53 @@ ui <- material_page(
 # b39ddb purple 3
 
 getColor <- function(Data, Type) {
-  col1 <- "#FFC312" # yellow #FFC312
-  col2 <- "#EA2027" # red #EA2027
-  col3 <- "#6F1E51" # purple #6F1E51
+  col1 <- "#FFC312" # yellow #FFC312 
+  col2 <- "#EE5A24" # orange #EE5A24
+  col3 <- "#EA2027" # red #EA2027
   colBasic <- "#A3CB38" # Green #A3CB38
-  if (Type == "Point") {
+  
+  if (Type == "TRI") {
     res <-
       sapply(Data[[Type]], function(i) {
         v <- colBasic
-        if (i > 5) v <- col1
-        if (i > 10) v <- col2
-        if (i > 15) v <- col3
+        if (i == 1) v <- col1
+        if (i == 2) v <- col2
+        if (i == 3) v <- col3
         return(v)
       }, USE.NAMES = FALSE)
   }
-  if (Type == "Temperature") {
+  if (Type == "T") {
     res <-
-      sapply(Data[[Type]], function(i) {
-        v <- colBasic
-        if (i <= 36) v <- col1
-        if (i >= 37.5) v <- col1
-        if (i > 38) v <- col2
-        if (i > 39) v <- col3
-        return(v)
+      sapply(Data[[Type]], function(T) {
+        if(T <= 35) return(col3)
+        if(T <= 36 || T >39) return(col2)
+        if(T >= 38 ) return(col1)
+        return(colBasic)
       }, USE.NAMES = FALSE)
   }
-  if (Type == "Oxygen") {
+  if (Type == "O") {
     res <-
-      sapply(Data[[Type]], function(i) {
-        v <- colBasic
-        if (i <= 95) v <- col1
-        if (i <= 93) v <- col2
-        if (i <= 91) v <- col3
-        return(v)
+      sapply(Data[[Type]], function(O) {
+        if(O<=93) return(col3)
+        if(O<=95) return(col2)
+        return(colBasic)
       }, USE.NAMES = FALSE)
   }
-  if (Type == "BreathCount") {
+  if (Type == "BC") {
     res <-
-      sapply(Data[[Type]], function(i) {
-        v <- colBasic
-        if (i <= 11) v <- col1
-        if (i <= 8) v <- col3
-        return(v)
+      sapply(Data[[Type]], function(BC) {
+        if(BC >=25 || BC <=8) return(col3)
+        if(BC >=21 || BC <=11) return(col2)
+        return(colBasic)
       }, USE.NAMES = FALSE)
   }
-  if (Type == "BloodPressure") {
+  if (Type == "P") {
     res <-
-      sapply(Data[[Type]], function(i) {
-        v <- colBasic
-        if (i <= 110) v <- col1
-        if (i <= 100) v <- col2
-        if (i <= 90) v <- col3
-        return(v)
+      sapply(Data[[Type]], function(P) {
+        if(P > 110 || P <= 40) return(col3)
+        if(P >= 100 ) return(col2)
+        if(P >= 101 || P <= 110) return(col2)
+        return(colBasic)
       }, USE.NAMES = FALSE)
   }
 
@@ -278,7 +311,6 @@ server <- function(input, output, session) {
     check_credentials = check_credentials("database.sqlite")
   )
 
-
   tab <- newtab <- ""
 
   output$tab1 <- renderDataTable({
@@ -289,34 +321,42 @@ server <- function(input, output, session) {
 
     tab <- readxl::read_excel(inFile$datapath)
 
-    tab$Sex <- as.factor(tab$Sex)
     tab$Occurrence <- as.Date(tab$Occurrence)
     tab$Confirm <- as.Date(tab$Confirm)
     tab$Date <- as.Date(tab$Date)
-
-    triages <- sapply(1:nrow(tab), function(i) {
+    tab$S <- as.factor(tab$S)
+    #tab$D <- as.factor(tab$D)
+    
+    TRIS <- sapply(1:nrow(tab), function(i) {
       triage(tab[i, ])
     })
+    rownames(TRIS) = c('PT','PCF','PCO','PM','TRI')
+    
+    tab = tab %>% cbind(t(TRIS))
+    
     # hide file upload
     shinyjs::runjs('$(".form-group.shiny-input-container").hide()')
 
-    tab$Point <- triages
     tab <<- tab
 
     newtab <<- tab %>%
-      filter(Date == max(Date)) %>%
-      select(-City, -Town, -Occurrence, -Confirm)
+      filter(Date == max(Date)) %>% # recent day
+      select(Place, Name, S, A, -City, -Town, -Occurrence, -Confirm, PT, PCF, PCO, PM, TRI)
 
-
+    ########################
+    # MERGING COLUMN  : 8  #
+    # ORDER COLUMN    : 8  #
+    ########################
+    
     dtobj <- datatable(
       newtab,
       escape = FALSE,
       caption = "전체 환자: 시설",
       options = list(
-        rowCallback = styleDT(3, 4, 5, 6, 7, 8, 9, 11),
+        #rowCallback = styleDT(3, 4, 5, 6, 7, 8, 9, 11),
         dom = "tip",
-        rowsGroup = list(0), # Place, not Sex!
-        order = list(list(10, "desc"))
+        rowsGroup = list(8), # TRIAGE
+        order = list(list(8, "desc"))
       ),
       selection = "single",
       filter = "top",
@@ -334,31 +374,51 @@ server <- function(input, output, session) {
 
   observeEvent(input$tab1_rows_selected, {
     selected <- input$tab1_rows_selected # check none selected
-    thisTab <- tab %>% filter(Name == newtab$Name[selected])
-
-    output$pat <- renderText(
-      paste0(
+    tt <- thisTab <- tab %>% filter(Name == newtab$Name[selected])
+    
+    # print(head(thisTab))
+    
+    output$pat <- renderText({
+      txt = paste0(
         HTML('<i class = "material-icons" style= "font-size : 2.5rem">face</i> '), # icon
         thisTab$Name[1], " / ", # name
-        thisTab$Sex[1], " / ", # sex
+        thisTab$S[1], " / ", # sex
         "Confirmed in ", thisTab$Confirm[1], " / ", # confirmed
         thisTab$Town[1], " / ", # town
-        thisTab$Place[1]
+        thisTab$Place[1], ' / '
       )
-    )
+      if(thisTab$TRI[1]==3) txt = paste0(txt,'상급병상우선')
+      if(thisTab$TRI[1]==2) txt = paste0(txt,'병상우선')
+      if(thisTab$TRI[1]<=1) txt = paste0(txt,'가정')
+      txt
+    })
 
     output$tab2 <- renderDataTable({
+      
+      thisTab = thisTab %>% select(-City, -Town, -Place, -Occurrence, -Confirm, -Name, -S, -A, -PT, -PCF, -PCO, -PM)
+      minus = c()
+      for(i in 3:ncol(thisTab)){
+        if(sum(thisTab[,i])==0) minus= c(minus, i)
+        thisTab[which(thisTab[,i]=='TRUE'),i] = 'T'
+        thisTab[which(thisTab[,i]=='FALSE'),i] = 'F'
+      }
+      
+      thisTab = thisTab[,-minus]
+      
+      TRIIDX = which(colnames(thisTab)=='TRI') - 1
+      DIDX = which(colnames(thisTab)=='D') - 1
+      
       dtobj <-
         datatable(
-          thisTab[, -(1:7)],
+          thisTab,
           rownames = FALSE,
           selection = "none",
           options = list(
-            rowsGroup = list(0, 1), # Age, Disease
-            rowCallback = styleDT(0, 1, 2, 3, 4, 5, 6, 8),
+            rowsGroup = list(DIDX), # Disease
+            #rowCallback = styleDT(0, 1, 2, 3, 4, 5, 6, 8),
             dom = "tip",
             autoWidth = FALSE,
-            order = list(list(7, "desc"))
+            order = list(list(TRIIDX, "desc"))
           )
         )
       path <- file.path(getwd(), "www")
@@ -372,24 +432,25 @@ server <- function(input, output, session) {
     })
 
     output$img <- renderHighchart({
+      thisTab = tt
       highchart() %>%
         hc_xAxis(type = "datetime", title = list(text = "Day")) %>%
         hc_yAxis_multiples(
-          list(top = "0%", height = "20%", title = list(text = "Point"), lineWidth = 3),
-          list(top = "20%", height = "20%", title = list(text = "Temp"), showFirstLabel = T, showLastLabel = T, opposite = T),
-          list(top = "40%", height = "20%", title = list(text = "SpO2"), showFirstLabel = T, showLastLabel = T),
-          list(top = "60%", height = "20%", title = list(text = "RR"), showFirstLabel = T, showLastLabel = T, opposite = T),
-          list(top = "80%", height = "20%", title = list(text = "SBP"), showFirstLabel = T, showLastLabel = T)
+          list(top = "0%", height = "20%", title = list(text = "TRI"), lineWidth = 3),
+          list(top = "20%", height = "20%", title = list(text = "T"), showFirstLabel = T, showLastLabel = T, opposite = T),
+          list(top = "40%", height = "20%", title = list(text = "O"), showFirstLabel = T, showLastLabel = T),
+          list(top = "60%", height = "20%", title = list(text = "BC"), showFirstLabel = T, showLastLabel = T, opposite = T),
+          list(top = "80%", height = "20%", title = list(text = "P"), showFirstLabel = T, showLastLabel = T)
         ) %>%
-        hc_add_series(getColor(thisTab, "Point"), "line", hcaes(Date, y, color = color), name = "Point", marker = list(radius = 8)) %>%
-        hc_add_series(getColor(thisTab, "Temperature"), "line", hcaes(Date, y, color = color), name = "Temp", marker = list(radius = 8), yAxis = 1) %>%
-        hc_add_series(getColor(thisTab, "Oxygen"), "line", hcaes(Date, y, color = color), name = "SpO2", marker = list(radius = 8), yAxis = 2) %>%
-        hc_add_series(getColor(thisTab, "BreathCount"), "line", hcaes(Date, y, color = color), name = "RR", marker = list(radius = 8), yAxis = 3) %>%
-        hc_add_series(getColor(thisTab, "BloodPressure"), "line", hcaes(Date, y, color = color), name = "SBP", marker = list(radius = 8), yAxis = 4) %>%
+        hc_add_series(getColor(thisTab, "TRI"), "line", hcaes(Date, y, color = color), name = "TRI", marker = list(radius = 8)) %>%
+        hc_add_series(getColor(thisTab, "T"), "line", hcaes(Date, y, color = color), name = "T", marker = list(radius = 8), yAxis = 1) %>%
+        hc_add_series(getColor(thisTab, "O"), "line", hcaes(Date, y, color = color), name = "O", marker = list(radius = 8), yAxis = 2) %>%
+        hc_add_series(getColor(thisTab, "BC"), "line", hcaes(Date, y, color = color), name = "BC", marker = list(radius = 8), yAxis = 3) %>%
+        hc_add_series(getColor(thisTab, "P"), "line", hcaes(Date, y, color = color), name = "P", marker = list(radius = 8), yAxis = 4) %>%
         hc_exporting(enabled = T) %>%
         hc_tooltip(valueDecimals = 1, shared = T, crosshairs = T)
     })
   })
 }
 
-shinyApp(ui = ui, server = server, options = list(launch.browser = TRUE))
+shinyApp(ui = ui(), server = server, options = list(launch.browser = TRUE))
